4b7c7b4cb10d4635137a55a434cfd95df8cf368f
strothmannsm <strothmann.sm@gmail.com>
2017-05-04 08:31:55 -0500

    Add C# Reflection Example

---
+++ Reflection/CSharpReflection/CSharpReflection/MyClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpReflection
+{
+    
+    class MyClass
+    {
+        private bool canYouSeeMe;
+        public int someNum;
+        public double someOtherNum;
+        public bool someOtherVariable;
+        private bool myPrivateVariable;
+
+        public MyClass()
+        {
+            someNum = 0;
+            someOtherNum = 0.0;
+            someOtherVariable = false;
+        }
+
+        public MyClass(int someNum, double someOtherNum, bool someOtherVariable)
+        {
+            this.someNum = someNum;
+            this.someOtherNum = someOtherNum;
+            this.someOtherVariable = someOtherVariable;
+        }
+
+        public int squareSomeNum()
+        {
+            return someNum * someNum;
+        }
+
+        public bool someOtherFunction(int someThing)
+        {
+            return someOtherVariable;
+        }
+    }
+}
---
+++ Reflection/CSharpReflection/CSharpReflection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace CSharpReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass mc = new MyClass();
+            Type mcType = mc.GetType();
+            Console.WriteLine("Type: " + mcType.ToString());
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            ConstructorInfo[] constructorInfo = mcType.GetConstructors();
+            foreach (var cinfo in constructorInfo)
+            {
+                Console.WriteLine("MyClass Constructor: " + cinfo.ToString());
+            }
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            MethodInfo[] methodInfo = mcType.GetMethods();
+            foreach (var minfo in methodInfo)
+            {
+                Console.WriteLine("MyClass Method: " + minfo.ToString());
+            }
+
+            Console.WriteLine("----------------------------------------------------------");
+
+            FieldInfo[] fieldInfo = mcType.GetFields();
+            foreach (var finfo in fieldInfo)
+            {
+                Console.WriteLine("MyClass Field: " + finfo.Name + " is a " + finfo.FieldType);
+            }
+
+            Console.WriteLine("----------------------------------------------------------");
+        }
+    }
+}
